f9b10835ea014f72fb4c65cd872a7620162aebd7
Roei <roei.webd@gmail.com>
2021-03-25 16:29:56 +0200

    first commit enzyme setup and data-test

---
+++ 01_click-counter/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div data-test="component-app" className="App">
+    <h1 data-test="counter-display">The counter is currently&nbsp;
+    <span data-test="count">{count}</span>
+    </h1>
+    <button onClick={()=>setCount(c=> c + 1)} data-test="increment-button">Increment counter</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01_click-counter/src/App.test.js
@@ -0,0 +1,51 @@
+import App from "./App";
+import Enzyme, { shallow, ShallowWrapper} from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+Enzyme.configure({ adapter: new Adapter() });
+
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @Function
+ * @returns { ShallowWrapper }
+ */
+const setup = () => {
+  return shallow(<App />);
+};
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+};
+
+test("render without error", () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
+
+});
+test("render increment button", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  expect(button.length).toBe(1);
+});
+
+test("render counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.length).toBe(1);
+});
+
+
+test("counter display starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+});
+
+test("clicking button incremenent counter display", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper , "increment-button");
+  button.simulate('click');
+  const count = findByTestAttr(wrapper, "count").text();
+
+  expect(count).toBe("1");
+});
