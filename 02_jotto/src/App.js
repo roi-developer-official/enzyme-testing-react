5fa4341022d7417444a21236877a491a61b31ce6
Roei <roei.webd@gmail.com>
2021-03-26 19:42:38 +0200

    basis for app component

--- 02_jotto/src/App.js
+++ 02_jotto/src/App.js
@@ -4,7 +4,7 @@ import Congrats from './Congrats';
 import GuessWords from './GuessWords';
 function App() {
   return (
-    <div className="App">
+    <div data-test="component-app" className="App">
       <h1>Jotto</h1>
       <Congrats sucess={false} />
       <GuessWords guessedWords={[ { guessedWord: 'train', letterMatchCount: 3 } ]} />
