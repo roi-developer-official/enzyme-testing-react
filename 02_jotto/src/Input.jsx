ef58f06fafaf4dd5f551b432069022b1f14e7e50
Roei <roei.webd@gmail.com>
2021-03-26 14:03:15 +0200

    added tests and code for input, tesing usestate

--- 02_jotto/src/Input.jsx
+++ 02_jotto/src/Input.jsx
@@ -1,12 +1,31 @@
 import PropType from 'prop-types';
-function Input({ secretWord }){
+import React from 'react';
 
+function Input({ secretWord }){
+    const [currentGuess, setCurrentGuess] = React.useState("");
     return (
-        <div data-test="component-input"></div>
+        <div data-test="component-input">
+            <form>
+                <input 
+                data-test="input-box" 
+                placeholder="enter guess"
+                value={currentGuess}
+                onChange={(e)=>setCurrentGuess(e.target.value)}
+                type="text"/>
+            <button 
+            onClick={(e)=>{
+                e.preventDefault();
+                setCurrentGuess("")
+            }}
+            data-test="submit-button">
+                Submit</button>
+            </form>
+        </div>
     )
 }
 
 Input.propTypes = {
     secretWord: PropType.string.isRequired
 }
+
 export default Input;
--- 02_jotto/src/Input.test.js
+++ 02_jotto/src/Input.test.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import { findByAttr, checkProp } from './tests/testUtils';
-import Input from './Input';
+import { findByAttr, checkProp } from "./tests/testUtils";
+import Input from "./Input";
 
-const setup = (secretWord = 'party')=>{
-    return shallow(<Input secretWord={secretWord} />);
-}
+const setup = (secretWord = "party") => {
+  return shallow(<Input secretWord={secretWord} />);
+};
 
-
-test('renders without error', ()=>{
-    const wrapper = setup();
-    const inputComponent = findByAttr(wrapper, 'component-input');
-    expect(inputComponent.length).toBe(1);
+test("renders without error", () => {
+  const wrapper = setup();
+  const inputComponent = findByAttr(wrapper, "component-input");
+  expect(inputComponent.length).toBe(1);
 });
 
-test('dont throw error when prop is currect', ()=>{
-    checkProp(Input, { secretWord : 'party' })
-})
+test("dont throw error when prop is currect", () => {
+  checkProp(Input, { secretWord: "party" });
+});
+
+describe("state controlled input field", () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  let originalUseState;
+
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    originalUseState = React.useState;
+    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+    wrapper = setup();
+  });
+
+  afterEach(()=>{
+    React.useState = originalUseState;
+  });
+
+  test("state updates with value of input box upon change", () => {
+    const inputBox = findByAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "train" } };
+    inputBox.simulate("change", mockEvent);
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+
+
+  test("field is clrear upon submit", () => {
+    const submitButton = findByAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault: ()=>{}});
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
+  });
+
+});
+
