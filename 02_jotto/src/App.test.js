344bdc690fccec2b272c547e5de898f76b6ded16
Roei <roei.webd@gmail.com>
2021-03-27 19:52:39 +0300

    added jest mock function and testd function called with use-effect

--- 02_jotto/src/App.test.js
+++ 02_jotto/src/App.test.js
@@ -1,9 +1,13 @@
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByAttr } from "./tests/testUtils";
 import App from "./App";
+import { getSecretWord as mockGetSecretWord } from './actions';
+
+//activate global mock
+jest.mock('./actions');
 
 const setup = (props = {}) => {
-  return shallow(<App />);
+  return mount(<App />);
 };
 
 test("renders without error", () => {
@@ -12,3 +16,26 @@ test("renders without error", () => {
   expect(app.length).toBe(1);
 });
 
+describe('get secret word', ()=>{
+
+  beforeEach(()=>{
+    mockGetSecretWord.mockClear()
+  });
+
+
+  test('getSecretWord on app mount', ()=>{
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test('getSecretWord not run on app update',()=>{
+    const wrapper = setup();
+    
+    //using wrapper wont trigger useEffect, 
+    wrapper.setProps() // this willl update the component.
+    
+    mockGetSecretWord.mockClear()
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+
+  })
+})
