6dbcfbcf1d7ca1b734a1b156493cec1edeef2a97
Roei <roei.webd@gmail.com>
2021-03-28 16:52:53 +0300

    added currectGues action creator test

--- 03_jotto_redux/src/actions/index.js
+++ 03_jotto_redux/src/actions/index.js
@@ -1,6 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+//jest mock module
+export const getSecretWord = () => {
+  return axios.get("http://localhost:3030").then((res) => res.data);
+};
 
-export const getSecretWord = () =>{
-    return axios.get('http://localhost:3030')
-    .then(res=>res.data);
+//redux action
+export const actionTypes = {
+  CORRECT_GUESS: "CURRECT_GUESS",
+};
+
+/**
+ * @function currectGuess
+ * @returns {object} - Action object with type 'CURRECT_GUESS'
+ */
+
+console.log(actionTypes.CORRECT_GUESS);
+export function currectGuess() {
+  return { 
+      type: actionTypes.CORRECT_GUESS
+  };
 }
--- 03_jotto_redux/src/actions/index.test.js
+++ 03_jotto_redux/src/actions/index.test.js
@@ -1,5 +1,15 @@
 import moxios from "moxios";
-import { getSecretWord } from "./";
+import { getSecretWord, currectGuess, actionTypes } from "./";
+
+describe("currectGuess", () => {
+  test("return an action type `CURRECT_GUESS`", () => {
+    const action = currectGuess();
+
+    // this is a deep equal not shallow the inside objecs as well
+    expect(action).toMatchObject({ type: actionTypes.CORRECT_GUESS });
+  });
+});
+
 describe("getSecretWord", () => {
   beforeEach(() => {
     moxios.install();
@@ -20,6 +30,5 @@ describe("getSecretWord", () => {
     return getSecretWord().then((secretWord) => {
       expect(secretWord).toBe("party");
     });
-    
   });
 });
